188abba30d2f7db7195e3c741fe3fbd680761725
“smgowthaman” <smgowthaman@gmail.com>
2025-09-19 15:23:04 +0000

    my info added

---
+++ provider.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+ # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "smgtest9629294284"
+    key    = "vgs/terraform.tfstate"
+    region = "us-east-1"   
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+locals {
+  region = "us-east-1"
+  name = "smg_cluster"
+  vpc_cidr = "10.123.0.0/16"
+  azs      = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
+  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+  tags = {
+    Example = local.name
+  }
+}
+
+
